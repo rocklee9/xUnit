3228cebdff6fe94a4e6a1e8cd2ed5ca4f4492bff
Minh Hoang <minhhoang97hk@gmail.com>
2020-05-13 10:59:29 +0700

    sử dụng xunit cơ bản

---
+++ xUnit_base/PrimeService/PrimeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrimeService
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate <2)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Not fully implemented.");
+        }
+    }
+}
